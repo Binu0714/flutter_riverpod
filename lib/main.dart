2d204d017b7935a2f06d48cd7a4b67d28dfcb4ab
Binu Jinajith <binuj714@gmail.com>
2026-05-08 12:24:34 +0530

    feat(basics): implement counter and theme providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'features/basics/providers/basics_providers.dart';
+import 'features/posts/presentation/pages/posts_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeModeProvider);
+
     return MaterialApp(
-      home: Scaffold(body: Center(child: Text("Day 2: Clean Architecture"))),
+      debugShowCheckedModeBanner: false,
+      themeMode: themeMode,
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      home: const PostsPage(),
     );
   }
 }
